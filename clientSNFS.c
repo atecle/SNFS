eb2c6762622835b7b1d3d890c4d20a58c81257c4
Adam <Tecle@adams-MBP.home>
2014-11-18 21:35:42 -0500

    initial commit

---
+++ clientSNFS.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+
+
+void setServer(char *serverIP, int port) {
+    
+}
+
+int openFile(char *name) {
+    return 0;
+}
+
+int readFile(int fd, void *buf) {
+    
+}
+
+int writeFile(int fd, void *buf) {
+    
+}
+
+int statFile(int fd, struct fileStat *buf) {
+    
+}
+
+int closeFile(int fd) {
+    
+}
